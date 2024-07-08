cb1855f9a34cba7da39e94312e868fdcef59d467
Sunjay Kalsi <sunjay.kalsi@gmail.com>
2024-07-08 09:26:59 +0100

    Initial Commit

---
+++ CatFacts/CatFactsApp.swift
@@ -0,0 +1,17 @@
+//
+//  CatFactsApp.swift
+//  CatFacts
+//
+//  Created by Sunjay Kalsi on 08/07/2024.
+//
+
+import SwiftUI
+
+@main
+struct CatFactsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
